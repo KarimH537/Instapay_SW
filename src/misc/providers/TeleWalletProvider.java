86832bb22ff968df962de07bf7852761f52f70db
OlyRSPS <42236845+OlyRSPS@users.noreply.github.com>
2023-11-15 01:51:45 +0200

    Implemented dummy missing methods in providers directory

--- src/misc/providers/TeleWalletProvider.java
+++ src/misc/providers/TeleWalletProvider.java
@@ -2,4 +2,13 @@ package misc.providers;
 
 public class TeleWalletProvider implements TransferProvider {
 
+    @Override
+    public Transaction transfer(String identifier, double amount, String senderUserName) {
+        return null;
+    }
+
+    @Override
+    public double getBalance(String identifier) {
+        return 0;
+    }
 }
--- src/misc/providers/WaterProvider.java
+++ src/misc/providers/WaterProvider.java
@@ -2,4 +2,13 @@ package misc.providers;
 
 public class WaterProvider implements ServiceProvider {
 
+    @Override
+    public double getBillAmount(String eCode) {
+        return 0;
+    }
+
+    @Override
+    public Bill payBill(String eCode) {
+        return null;
+    }
 }
