61c6b0e816ca1b62eb42597bbbac8efbbea9c54c
OlyRSPS <42236845+OlyRSPS@users.noreply.github.com>
2023-11-15 22:08:00 +0200

    Updated registering functionallity to set the identifiers of the sources

--- src/views/RegisterView.java
+++ src/views/RegisterView.java
@@ -13,64 +13,6 @@ public class RegisterView {
     private static Scanner scanner = new Scanner(System.in);
 
     public static void showRegisterView() {
-        System.out.println("1. Register using bank account\n2. Register using wallet\n3. Back\n4. Exit\n");
-        int option = scanner.nextInt();
-        scanner.nextLine();
-        switch (option) {
-            case 1:
-                registerUser(new BankAccount());
-                break;
-            case 2:
-                registerWalletUser();
-                break;
-            case 3:
-                break;
-            default:
-                System.exit(0);
-        }
-    }
-
-    private static void registerWalletUser() {
-        System.out.println("1. Register using tele wallet\n2. Register using bank wallet\n3. Register using epay wallet\n4. Back\n");
-        int option = scanner.nextInt();
-        scanner.nextLine();
-        switch (option) {
-            case 1:
-                registerUser(new TeleWallet());
-                break;
-            case 2:
-                registerUser(new BankWallet());
-                break;
-            case 3:
-                registerUser(new EpayWallet());
-                break;
-            case 4:
-                break;
-            default:
-                System.exit(0);
-        }
-    }
-
-
-    // Validate username (at least 4 characters)
-    private static boolean isValidUsername(String username) {
-
-        return username.length() >= 4;
-    }
-
-    // Validate password
-    // 8 characters + 1 digit + lowercase char + uppercase char + one special character
-    private static final String PASSWORD_PATTERN =
-            "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,20}$";
-
-    private static final Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
-
-    public static boolean isValidPassword(final String password) {
-        Matcher matcher = pattern.matcher(password);
-        return matcher.matches();
-    }
-
-    private static boolean registerUser(FundingSource source) {
         //Prompt for input
         System.out.print("Enter username:\n");
         String username = scanner.nextLine();
@@ -93,7 +35,49 @@ public class RegisterView {
         System.out.print("Enter phone number:\n");
         String phoneNumber = scanner.nextLine();
 
-        return UserManager.registerUser(username, password, phoneNumber, source);
+        System.out.println("1. Register using bank account\n2. Register using tele wallet\n3. Register using bank wallet\n" +
+                "4. Register using epay wallet\n5. Back\n");
+        int option = scanner.nextInt();
+        scanner.nextLine();
+        switch (option) {
+            case 1:
+                System.out.print("Enter bank IBAN:\n");
+                String accNum = scanner.nextLine();
+                //Check if valid IBAN using API
+                UserManager.registerUser(username, password, phoneNumber, new BankAccount(accNum));
+                break;
+            case 2:
+                UserManager.registerUser(username, password, phoneNumber, new TeleWallet(phoneNumber));
+                break;
+            case 3:
+                UserManager.registerUser(username, password, phoneNumber, new BankWallet(phoneNumber));
+                break;
+            case 4:
+                UserManager.registerUser(username, password, phoneNumber, new EpayWallet(phoneNumber));
+                break;
+            case 5:
+                break;
+            default:
+                System.exit(0);
+        }
+    }
+
+    // Validate username (at least 4 characters)
+    private static boolean isValidUsername(String username) {
+
+        return username.length() >= 4;
+    }
+
+    // Validate password
+    // 8 characters + 1 digit + lowercase char + uppercase char + one special character
+    private static final String PASSWORD_PATTERN =
+            "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,20}$";
+
+    private static final Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
+
+    public static boolean isValidPassword(final String password) {
+        Matcher matcher = pattern.matcher(password);
+        return matcher.matches();
     }
 
     public static boolean showLoginView() {
