7a23794771e019f67cb360ce23b4f97dabdd0beb
Karim <karimhassib537@gmail.com>
2023-11-15 23:28:18 +0200

    Added testMethod

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import managers.TransactionManager;
 import managers.UserManager;
 import misc.Database;
+import misc.providers.services.GasProvider;
+import models.User;
+import models.sources.BankAccount;
+import models.sources.BankWallet;
+import models.sources.EpayWallet;
 import views.UserView;
 import views.TransactionView;
 
@@ -7,11 +13,30 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {//TODO
+    public static void testMethod() {
+        // Register
+        UserManager.registerUser("Boda", "Bodapk1!@#", "0101001010", new BankAccount("AE10000"));
+        UserManager.registerUser("Koghwassioant", "Bodapk1!@#", "0101001012", new EpayWallet("0101001012"));
+        // Login
+        UserManager.loginUser("Boda", "Bodapk1!@#");
+        // Transfer to User
+        TransactionManager.transferToUser(UserManager.getCurrentUser(), "Koghwassioant", 100);
+        // Print transactions
+        TransactionView.printUserTransactions();
+        // View profile
+        TransactionView.printUserProfile();
+        // Pay Bill
+        TransactionManager.payBill(UserManager.getCurrentUser(), "E10234900", new GasProvider());
+    }
+
+    public static void main(String[] args) {
         Database.init();
         System.out.println("Welcome to Instapay (Rady Edition).\nPlease choose the option you would like to do:\n");
+        /*
         while (true)
             showMenu();
+        */
+        testMethod();
     }
 
     public static void showMenu() {
